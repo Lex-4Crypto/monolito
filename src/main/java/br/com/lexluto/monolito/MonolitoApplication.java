ebfb618e8972e016e10d7247e6e80180d8d5c582
Victor-Acrani <victoracrani@gmail.com>
2022-07-27 22:46:12 -0300

    First commit Lex Luto.

---
+++ src/main/java/br/com/lexluto/monolito/MonolitoApplication.java
@@ -0,0 +1,15 @@
+package br.com.lexluto.monolito;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class MonolitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolitoApplication.class, args);
+	}
+
+}
